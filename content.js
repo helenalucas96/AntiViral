c8a009cd15848ee0ddc7117d49a2a05c3faf9af3
helenalucas96 <helena.lucas96@gmail.com>
2020-05-23 19:16:12 +0100

    i lied

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/coronavirus/g, 'puppies').replace(/Coronavirus/g, 'Puppies').replace(/COVID/gi, 'a CAT').replace(/virus|infection/gi, 'kitten').replace(/lockdown/gi, 'hibernation').replace(/isolation/gi,'relaxing alone time').replace(/pandemic/gi, 'flowers').replace(/quarantine/gi, 'chilling');
+            var replacedText = text.replace(/coronavirus/g, 'puppies').replace(/Coronavirus/g, 'Puppies').replace(/COVID/gi, 'a CAT').replace(/virus|infection/gi, 'kitten').replace(/lockdown/gi, 'hibernation').replace(/isolation/gi,'relaxing alone time').replace(/pandemic/gi, 'flowers').replace(/quarantine/gi, 'chillaxing');
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
